0de10d7ce3d0cfcb93108c53b5bf327434931f6d
osexpert <33638596+osexpert@users.noreply.github.com>
2026-08-15 18:41:05 +0200

    renames

--- src/config_list.h
+++ src/config_list.h
@@ -37,7 +37,7 @@ CONFIG_ITEM(CHADEMO_FAKE_IDLE_AMPS, "fake-idle-amps", uint8_t, 1);
 // car seems to allows 20V deviation. Adding +- 20V in addition should allow 40V deviation. +-30V also worked, but if +-20V works, lets keep +-30 as backup:-) But 0 is default (no modulation).
 CONFIG_ITEM(CHADEMO_VOLTAGE_MODULATION, "voltage-modulation", uint8_t, 0);
 
-CONFIG_ITEM(DX_CCS_WaitForPreChargeStart_MS, "wait-for-precharge-start-ms", uint16_t, 2000);
+CONFIG_ITEM(DX_CCS_WaitForPreChargeStart_MS, "precharge-start-delay-ms", uint16_t, 2000);
 
 CONFIG_ITEM(CHADEMO_MAX_UNDERSUPPLY_AMPS, "max-undersupply-amps", uint8_t, 10);
 
@@ -51,4 +51,4 @@ CONFIG_ITEM(CONFIG_CHADEMO_09, "chademo-0.9", bool, false);
 
 // Pro: voltage will drop consistently and hopefully faster? Won't rely on every chargers bleed-down speed?
 // Con: inlet wont bleed-down at all? But spec demand less than 1uF, but still...charge could be "trapped" inside the inlet and voltage never drop?
-CONFIG_ITEM(CONFIG_OPEN_ADAPTER_CONTACTOR_BEFORE_WELDING_DETECTION, "open-adapter-contactor-before-wd", bool, false);
+CONFIG_ITEM(CONFIG_OPEN_ADAPTER_CONTACTOR_BEFORE_WELDING_DETECTION, "open-adapter-contactor-before-welding-detection", bool, false);
